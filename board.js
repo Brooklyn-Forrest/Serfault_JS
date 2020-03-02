0785b988360fb583ee47a97d0ee5dbbad785e1a7
Brooklyn-Forrest <45461267+Brooklyn-Forrest@users.noreply.github.com>
2020-03-02 14:22:35 -0600

    Update board.js

--- board.js
+++ board.js
@@ -8,10 +8,10 @@ function generate_board(append_frame){
     tile_x = 0;
     tile_y = 0;
 
-    while(tile_y <= 8){
-        while(tile_x <= 8){
+    while(tile_x <= 8){
+        while(tile_y <= 8){
             tile_div = document.createElement('div');
-            tile_div.id = "(" + tile_x + ", " + tile_y + ")";
+            tile_div.id = "[" + tile_x + ", " + tile_y + "]";
             tile_div.className = "tile";
             if(color_switch === 0){
                 tile_div.style = "background-color: black";
@@ -22,10 +22,10 @@ function generate_board(append_frame){
                 color_switch = 0
             }
             div_container.appendChild(tile_div);
-            tile_x = tile_x + 1;
+            tile_y = tile_y + 1;
         }
-        tile_x = 0;
-        tile_y = tile_y + 1;
+        tile_y = 0;
+        tile_x= tile_x + 1;
     }
-    tile_y = 0;
+    tile_x = 0;
 }
