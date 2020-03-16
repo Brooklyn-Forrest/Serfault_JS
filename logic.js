7f15a28b77806d626c65b5c45570bcd39941352f
Brooklyn-Forrest <45461267+Brooklyn-Forrest@users.noreply.github.com>
2020-03-16 08:05:40 -0500

    Update logic.js

--- logic.js
+++ logic.js
@@ -11,16 +11,20 @@ spaces = [[[0, 0], 0], [[0, 1], 0], [[0, 2], 0], [[0, 3], 0], [[0, 4], 0], [[0, 
           [[8, 0], 0], [[8, 1], 0], [[8, 2], 0], [[8, 3], 0], [[8, 4], 0], [[8, 5], 0], [[8, 6], 0], [[8, 7], 0], [[8, 8], 0]
     ];
 
+spaces_copy = $.extend({}, spaces);
+
 // initial piece selected, if any
-selected_init = 0;
-selected_img = 0;
+// div?
+var selected_init = 0;
+// img on screen in selected div
+var selected_img = 0;
 
 // reference to index of spaces. [[x,y], z]
-selected_action_space = 0;
+var selected_action_space = 0;
 
 class Piece {
 
-    init(attack_dir, attack_dist, start_pos, movement_dist, dist_increment) {
+    init(attack_dir, attack_dist, start_pos, movement_dist, dist_increment, player) {
         // This string var is a way to identify which
         // directions relative to the piece it can attack.
         this.attack_dir = attack_dir;
@@ -35,46 +39,47 @@ class Piece {
         this.dist_increment = dist_increment;
         this.current_pos = this.start_pos;
         // set starting space as being occupied with piece
-        spaces[start_pos[0] * 8 + start_pos[1]][1] = this;
+        spaces[start_pos[0] * 9 + start_pos[1]][1] = this;
+        this.player = player;
     }
 }
 
 
 class Far extends Piece{
     // These are versatile movement pieces that cannot attack unless attack distance is 2.
-    constructor(start_pos){
-        super().init("Bent", 2, start_pos, 2, -1)
+    constructor(start_pos, player){
+        super().init("Bent", 2, start_pos, 2, -1, player)
     }
 }
 
 class Rider extends Piece {
     // These pieces can move in any direction up to two spaces. When attacking, the attack distance
     // must be 2 (pierce).
-    constructor(start_pos) {
-        super().init("Uni", 2, start_pos, 2, -1)
+    constructor(start_pos, player) {
+        super().init("Uni", 2, start_pos, 2, -1, player)
     }
 }
 
 class Barricade extends Piece{
     // These pieces must be attached by two pieces during the same turn to be destroyed.
     // It can only be moved one space at a time in cardinal directions and cannot attack.
-    constructor(start_pos) {
-        super().init("Cardinal", 0, start_pos, 1, -1)
+    constructor(start_pos, player) {
+        super().init("Cardinal", 0, start_pos, 1, -1, player)
     }
 }
 
 class FootSoldier extends Piece{
     // The footsoldier is a simple, short movement piece that can attack at 1 distance.
-    constructor(start_pos) {
-        super().init("Bent", 1, start_pos, 1, -1)
+    constructor(start_pos, player) {
+        super().init("Bent", 1, start_pos, 1, -1, player)
     }
 }
 
 class Ninja extends Piece{
     // The ninja can bypass barricade (but cannot attack it). It can attack at 2 or less distance.
     // Normal movement is one tile at a time.
-    constructor(start_pos) {
-        super().init("Uni", -2, start_pos, 1, -1)
+    constructor(start_pos, player) {
+        super().init("Uni", -2, start_pos, 1, -1, player)
     }
 }
 
@@ -82,16 +87,16 @@ class Spinner extends Piece{
     // The spinner can be spun in any direction and can charge direction types only when hitting a boundary.
     // It can attack at 3 or less distance, but any further and it simply gets stopped by anything else
     // that was 4 or more distance away.
-    constructor(start_pos) {
-        super().init("Bent-Edge", -3, start_pos, 5, 5)
+    constructor(start_pos, player) {
+        super().init("Bent-Edge", -3, start_pos, 5, 5, player)
     }
 }
 
 class Wraith extends Piece{
     // Your pieces can go through a wraith. Opponents cannot pass through a wraith and "stop short".
     // If a piece is stopped short because of wraith, wraith cannot attack the next turn.
-    constructor (start_pos) {
-        super().init("Uni", -2, start_pos, 2, 1)
+    constructor (start_pos, player) {
+        super().init("Uni", -2, start_pos, 2, 1, player)
     }
 }
 
@@ -101,8 +106,8 @@ class Brave extends Piece{
     // the ability martyr. Martyr is triggered when the brave is killed.
     // Martyr: If the divinity is still alive, it transforms into a spinner.
     // A wraith is created where the brave died.
-   constructor(start_pos) {
-       super().init("Uni", 3, start_pos, 3, -1)
+   constructor(start_pos, player) {
+       super().init("Uni", 3, start_pos, 3, -1, player)
    }
 }
 
@@ -110,8 +115,8 @@ class Brave extends Piece{
 class Divinity extends Piece{
     // The divinity has 2 regular movement of forced direction.
     // However, it attacks from exactly four distance away.
-    constructor(start_pos) {
-        super().init("Uni", 4, start_pos, 2, -2)
+    constructor(start_pos, player) {
+        super().init("Uni", 4, start_pos, 2, -2, player)
     }
 }
 
@@ -135,6 +140,7 @@ class Game {
             console.log(this.mode);
             this.status = false;
         }
+        this.turn = "p1";
     }
 
     // Dice function that determines who goes first.
@@ -160,6 +166,9 @@ class Game {
     attack(obj_piece, total_dist) {
         // If attack_dist is a negative number, a piece can attack from up to the attack_dist or less. If
         // attack_dist is instead a positive number, it MUST attack from the exact value of attack_dist.
+        if(obj_piece.player === selected_action_space[1].player){
+            return false;
+        }
         if (obj_piece.attack_dist > 0) {
             return obj_piece.attack_dist === total_dist;
         } else {
@@ -168,6 +177,9 @@ class Game {
     }
 
     select_space(obj_piece) {
+        if(selected_init.player !== this.turn){
+            return false;
+        }
         if(selected_action_space === 0){
             // no secondary space selected
             // highlight spaces that piece can go to?
@@ -181,23 +193,20 @@ class Game {
         }
         else {
             // selected_action_space (abstract selected values from spaces array) = [[(0), (1)], (1)]
-            let calculated_dist_x = Math.abs(obj_piece.current_pos[0] - selected_action_space[0][0]);
-            let calculated_dist_y = Math.abs(obj_piece.current_pos[1] - selected_action_space[0][1]);
+            let calculated_dist_x = Math.abs(selected_init.current_pos[0] - selected_action_space[0][0]);
+            let calculated_dist_y = Math.abs(selected_init.current_pos[1] - selected_action_space[0][1]);
             let total_dist = calculated_dist_x + calculated_dist_y;
+            console.log(total_dist);
 
             // If z !== 0 (empty) where [x, y], z
             var result = null;
             if (selected_action_space[1] !== 0) {
-                result = this.attack(obj_piece, total_dist)
+                result = this.attack(selected_init, total_dist)
             } else {
-                result = this.move(obj_piece, total_dist)
+                result = this.move(selected_init, total_dist)
             }
 
             if (result) {
-                obj_piece.current_pos = [selected_action_space[0][0], selected_action_space[0][1]];
-                spaces[obj_piece.current_pos[0] + obj_piece.current_pos[1]][1] = obj_piece;
-                // reset move selection vars
-                selected_init = 0;
                 return true;
             } else {
                 // reset move selection vars
@@ -205,18 +214,6 @@ class Game {
                 return false;
             }
         }
-    }
-
-    // This function takes the id of the tile div as an argument and translates it to a list.
-    // It then matches that list to the appropriate spot in the spaces array and initiates move
-    // validity calculation.
-    select_action(obj_piece, tile_value) {
-        // [x, y]
-        let coordinates = tile_value.split(",");
-        let index = coordinates[0] * 8 + coordinates[1];
-
-        var selected_space_abst = spaces[index];
-        return this.select_space(obj_piece, selected_space_abst);
     }
 
     generate_piece(type, tile_id, color, item_num){
@@ -297,64 +294,76 @@ class Game {
 
     init_game(){
         // p1 pieces
-        new Far([7, 0]);
+        new Far([7, 0], "p1");
         this.generate_piece("Far", "[7, 0]", this.p1color, 1);
-        new Far([7, 8]);
+        new Far([7, 8], "p1");
         this.generate_piece("Far", "[7, 8]", this.p1color, 2);
-        new FootSoldier([7, 1]);
+        new FootSoldier([7, 1], "p1");
         this.generate_piece("FootSolider", "[7, 1]", this.p1color, 1);
-        new FootSoldier([7, 7]);
+        new FootSoldier([7, 7], "p1");
         this.generate_piece("FootSolider", "[7, 7]", this.p1color, 2);
-        new Rider([8, 1]);
+        new Rider([8, 1], "p1");
         this.generate_piece("Rider", "[8, 1]", this.p1color, 1);
-        new Rider([8, 7]);
+        new Rider([8, 7], "p1");
         this.generate_piece("Rider", "[8, 7]", this.p1color, 2);
-        new Barricade([6, 4]);
+        new Barricade([6, 4], "p1");
         this.generate_piece("Barricade", "[6, 4]", this.p1color, 1);
-        new Ninja([8, 4]);
+        new Ninja([8, 4], "p1");
         this.generate_piece("Ninja", "[8, 4]", this.p1color, 1);
-        new Brave([8, 5]);
+        new Brave([8, 5], "p1");
         this.generate_piece("Brave", "[8, 5]", this.p1color, 1);
-        new Divinity([8, 3]);
+        new Divinity([8, 3], "p1");
         this.generate_piece("Divinity", "[8, 3]", this.p1color, 1);
-        new Wraith([7, 6]);
+        new Wraith([7, 6], "p1");
         this.generate_piece("Wraith", "[7, 6]", this.p1color, 1);
-        new Spinner([7, 2]);
+        new Spinner([7, 2], "p1");
         this.generate_piece("Spinner", "[7, 2]", this.p1color, 1);
 
         // p2 pieces
-        new Far([1, 0]);
+        new Far([1, 0], "p2");
         this.generate_piece("Far", "[1, 0]", this.p2color, 1);
-        new Far([1, 8]);
+        new Far([1, 8], "p2");
         this.generate_piece("Far", "[1, 8]", this.p2color, 2);
-        new FootSoldier([1, 1]);
+        new FootSoldier([1, 1], "p2");
         this.generate_piece("FootSolider", "[1, 1]", this.p2color, 1);
-        new FootSoldier([1, 7]);
+        new FootSoldier([1, 7], "p2");
         this.generate_piece("FootSolider", "[1, 7]", this.p2color, 2);
-        new Rider([0, 1]);
+        new Rider([0, 1], "p2");
         this.generate_piece("Rider", "[0, 1]", this.p2color, 1);
-        new Rider([0, 7]);
+        new Rider([0, 7], "p2");
         this.generate_piece("Rider", "[0, 7]", this.p2color, 2);
-        new Barricade([2, 4]);
+        new Barricade([2, 4], "p2");
         this.generate_piece("Barricade", "[2, 4]", this.p2color, 1);
-        new Ninja([0, 4]);
+        new Ninja([0, 4], "p2");
         this.generate_piece("Ninja", "[0, 4]", this.p2color, 1);
-        new Brave([0, 5]);
+        new Brave([0, 5], "p2");
         this.generate_piece("Brave", "[0, 5]", this.p2color, 1);
-        new Divinity([0, 3]);
+        new Divinity([0, 3], "p2");
         this.generate_piece("Divinity", "[0, 3]", this.p2color, 1);
-        new Wraith([1, 6]);
+        new Wraith([1, 6], "p2");
         this.generate_piece("Wraith", "[1, 6]", this.p2color, 1);
-        new Spinner([1, 2]);
+        new Spinner([1, 2], "p2");
         this.generate_piece("Spinner", "[1, 2]", this.p2color, 1);
     }
 
     select_tile(id){
         console.log("Selected tile.");
+        console.log(id);
+
+        let par_dom = document.getElementById(id);
         var coordinates = id.split(",");
-        let index = coordinates[0] * 8 + coordinates[1];
-        var test = id.getElementsByTagName('img');
-        if(test[0] && selected_init === 0){
+        coordinates[0] = coordinates[0].replace('[', '');
+        coordinates[1] = coordinates[1].replace(']', '');
+        coordinates[1] = coordinates[1].replace(' ', '');
+        console.log(coordinates);
+
+        let index = parseInt(coordinates[0]) * 9 + parseInt(coordinates[1]);
+        console.log(index);
+
+        // Check if img (piece) in div
+        var test = par_dom.getElementsByTagName('img');
+        // piece exists but is being initially selected, check for player turn
+        if(test[0] && selected_init === 0 && spaces[index][1].player === this.turn){
             // piece in selection and initial selection has not been made yet.
             selected_init = spaces[index][1];
             selected_img = test[0];
@@ -371,23 +380,40 @@ class Game {
             if(selected_action_space[1] !== 0) {
                 test[0].remove();
                 // create piece at bottom of board here
+            }
 
-                // move initially selected piece to new space
-                selected_action_space[1] = selected_init;
+            // update spaces with position change
+            // empty previous space
+            spaces[selected_init.current_pos[0] * 9 + selected_init.current_pos[1]][1] = 0;
+            // set new position
+            selected_init.current_pos = [selected_action_space[0][0], selected_action_space[0][1]];
+            // update spaces
+            spaces[selected_init.current_pos[0] * 9 + selected_init.current_pos[1]][1] = selected_init;
+            // move initially selected piece to new space
+            selected_action_space[1] = selected_init;
+            // reset move selection vars
+            selected_init = 0;
 
-                // copy properties of image
-                var cpy_id = selected_img.id;
-                var cpy_src = selected_img.src;
-                selected_img.remove();
+            // copy properties of image
+            var cpy_id = selected_img.id;
+            var cpy_src = selected_img.src;
+            selected_img.remove();
 
-                // recreate image render
-                let reimage = document.createElement('img');
-                reimage.className = "piece";
-                reimage.id = cpy_id;
-                reimage.src = cpy_src;
-                var div = document.getElementById('"'+selected_action_space[0]+'"');
-                div.appendChild(reimage);
-                selected_action_space = 0;
+            // recreate image render
+            let reimage = document.createElement('img');
+            reimage.className = "piece";
+            reimage.id = cpy_id;
+            reimage.src = cpy_src;
+            var div = document.getElementById('['+selected_action_space[0][0]+', ' + selected_action_space[0][1] + ']');
+            div.appendChild(reimage);
+            selected_action_space = 0;
+
+            // switch turns
+            if(this.turn === "p1"){
+                this.turn = "p2";
+            }
+            else{
+                this.turn = "p1";
             }
         }
         else{
@@ -400,11 +426,28 @@ class Game {
     init_interface(){
         let board = document.getElementById('board_container');
         let tiles = board.getElementsByTagName('div');
-
+        let ref = this;
         for(var tile of tiles){
-            tile.addEventListener('click', this.select_tile(tile.id))
+            tile.addEventListener('click', function(){
+                wrapper(ref, this)
+            })
         }
     }
+}
+
+function wrapper(inst, tile){
+    if (game_active){
+        inst.select_tile(tile.id)
+    }
+}
+
+function reset_game(inst){
+    selected_init = 0;
+    selected_img = 0;
+
+    selected_action_space = 0;
+    spaces = spaces_copy;
+    inst.delete_self();
 }
 
 
